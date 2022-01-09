7dba646ec97a55bcfa500adcdd16d50889f89096
BisiDavi <oludavidconnect@gmail.com>
2022-01-10 00:42:24 +0100

    blog updated

--- config/plugins.js
+++ config/plugins.js
@@ -1,14 +1,17 @@
-module.exports = ({ env }) => ({
-  upload: {
-    provider: "cloudinary",
-    providerOptions: {
-      cloud_name: env("CLOUDINARY_NAME"),
-      api_key: env("CLOUDINARY_KEY"),
-      api_secret: env("CLOUDINARY_SECRET"),
+module.exports = ({ env }) => {
+  console.log("module env", env, env("CLOUDINARY_NAME"), env("CLOUDINARY_KEY"));
+  return {
+    upload: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
     },
-    actionOptions: {
-      upload: {},
-      delete: {},
-    },
-  },
-});
+  };
+};
